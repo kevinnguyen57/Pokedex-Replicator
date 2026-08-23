90a0ff12a92038901c68330a8cf6830954a79056
kevinnguyen57 <kevinnguyen965@gmail.com>
2026-08-23 11:09:48 -0700

    looping through each pokemon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 /* Fetching Poke API v2 data */
 async function getPokemon() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0");
     /* -- Basic API call
     .then(response => response.json())  // json parses the API into readable data (converts response to JavaScript Object)
     .then(data => {
@@ -10,7 +10,9 @@ async function getPokemon() {
        -- Use this API call to make it easier making a pokedex */
     const data = await response.json();
 
-    console.log(data);
-}
+    // console.log(data); -- shows that fetching worked, all data in inspect
 
-getPokemon();
+    for (const pokemon of data.results) {
+        console.log(pokemon.name);
+    }
+}
