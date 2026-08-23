21fee1a8e7a180058ed94019820bd981f7c6a6ca
kevinnguyen57 <kevinnguyen965@gmail.com>
2026-08-23 11:01:16 -0700

    get pokemon works

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 /* Fetching Poke API v2 data */
 async function getPokemon() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
-    .then(response => response.json())
+    /* -- Basic API call
+    .then(response => response.json())  // json parses the API into readable data (converts response to JavaScript Object)
     .then(data => {
         console.log(data);
     });
+       -- Use this API call to make it easier making a pokedex */
+    const data = await response.json();
+
+    console.log(data);
 }
+
+getPokemon();
