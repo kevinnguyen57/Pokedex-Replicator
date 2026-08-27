03ce65305817fb0aada6dec7ff66f464f202aab1
kevinnguyen57 <kevinnguyen965@gmail.com>
2026-08-26 18:51:26 -0700

    added pokemon card and separate from container

--- pokedex-replicator/index.js
+++ pokedex-replicator/index.js
@@ -45,6 +45,7 @@ async function getPokemonData(url) {
 // Create current pokemon card given their url
 function createPokemonCard(pokemonData) {
     const card = document.createElement("div"); // Creates a new div
+    card.classList.add("pokemon-card");
 
     // We use map to go through each type the pokemon has in the array
     // We then separate each type then join them and put it in innerHTML
